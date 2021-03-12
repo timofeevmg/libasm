83a7817d7e5f137db5722ec88f657d4d1053f2dd
Elwanda Pilar <epilar@er-d1.kzn.21-school.ru>
2021-03-12 11:13:54 +0300

    strcpy

---
+++ ft_strcpy.s
@@ -0,0 +1,27 @@
+global _ft_strcpy
+
+section .text
+_ft_strcpy:
+	xor		rax, rax
+	xor		rcx, rcx
+	cmp		rsi, 0
+	je		.exit
+	cmp		rdi, 0
+	je		.exit
+
+.loop:
+	cmp		byte [rsi + rcx], 0
+	je		.return
+	mov		dl, [rsi + rcx]
+	mov		[rdi + rcx], dl
+	inc		rcx
+	jmp		.loop
+
+.return:
+	mov		byte [rdi + rcx], 0
+	mov		rax, rdi
+	ret
+
+.exit:
+	mov		rax, rdi
+	ret
--- main.c
+++ main.c
@@ -6,22 +6,41 @@
 /*   By: epilar <epilar@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/03/11 09:36:42 by epilar            #+#    #+#             */
-/*   Updated: 2021/03/11 11:59:04 by epilar           ###   ########.fr       */
+/*   Updated: 2021/03/12 11:12:34 by epilar           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
+#include <stdlib.h>
 
 int		ft_strlen(char *s);
+char	*ft_strcpy(char *dst, const char *src);
+
 
 int		main(void)
 {
+//strlen
 	char	*s1;
 	char	*s2;
+	char	*s3;
 
+//strcpy
+	char	*dst;
+	char	*src;
+
+
+//strlen
 	s1 = "hello";
 	s2 = "";
+	s3 = NULL;
 	printf("s1 length= %d | must be 5\n", ft_strlen(s1));
 	printf("s2 length= %d | must be 0\n", ft_strlen(s2));
+	printf("s3 length= %d | must be 0\n", ft_strlen(s3));
+
+//strcpy
+	src = "hello";
+	dst = (char *)malloc(6);
+	printf("dst: %s | must be hello\n", ft_strcpy(dst, src));
+
 	return (0);
 }
